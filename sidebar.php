c7338278a54b7175cdc7625dd98270cad1406a96
Will <will@Mancub.local>
2010-11-17 16:35:09 -0700

    Removed some classes from the sidebar.php file

--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,14 @@
-<h2 class="din">Subscribe</h2>
+<h2>Subscribe</h2>
 <ul>
 	<li><a href="/?feed=rss2">Entries (RSS)</a></li>
 	<li><a href="/?feed=comments-rss2">Comments (RSS)</a></li>
 </ul>
-<h2 class="din">Categories</h2>
+<h2>Categories</h2>
 <ul>
 	<?php wp_list_categories(array('title_li' => '')); ?> 
 </ul>
 
-<h2 class="din">Archives</h2>
+<h2>Archives</h2>
 <ul>
 	<?php wp_get_archives(array('type' => 'monthly')); ?>
 </ul>
